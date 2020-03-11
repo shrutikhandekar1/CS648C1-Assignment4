a09b30d5bbb8c2939972c494e19bdd7643642d6e
Shruti <katdareshruti@yahoo.com>
2020-03-10 20:09:25 -0700

    Added graphql extension to window.ENV.UI_API_ENDPOINT

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -123,7 +123,7 @@ class ProductAdd extends React.Component{
 
 async function graphQLFetch(query, variables = {}) {
     try {
-      const response = await fetch(window.ENV.UI_API_ENDPOINT, {
+      const response = await fetch(window.ENV.UI_API_ENDPOINT + "/graphql", {
         method: 'POST',
         headers: { 'Content-Type':'application/json'},
         body: JSON.stringify({ query, variables })
